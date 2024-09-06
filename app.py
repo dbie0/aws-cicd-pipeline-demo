d6f19707229acf5801f577d7ccc77f952776f1d0
D B <flyaway.receipt0v@icloud.com>
2024-09-06 16:35:40 +0200

    testing ApplicationStop 2

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World! This is a CI/CD pipeline demo with AWS CodePipeline. Version 1'
+    return 'Hello, World! This is a CI/CD pipeline demo with AWS CodePipeline. Version 2'
 
 
 if __name__ == '__main__':
